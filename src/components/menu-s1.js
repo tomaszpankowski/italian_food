ebc3a1f580c4afe024cfea624b4b6ec87d6edae4
tomaszpankowski <pankowski.email@gmail.com>
2022-03-23 15:07:26 +0100

    frontend

--- src/components/menu-s1.js
+++ src/components/menu-s1.js
@@ -6,10 +6,10 @@ import Col from "../../node_modules/react-bootstrap/Col";
 class MenuS1 extends Component{
     render(){
         return(    
-            <Container fluid className="d-flex minh-50vh align-items-center py-5">
+            <Container fluid className="menu-s1 d-flex minh-50vh align-items-center py-5">
                 <Row className="mx-0 w-100 mt-5">
-                    <Col xs={12} md={6} className="minh-25vh d-none d-md-block"></Col>
-                    <Col xs={12} md={6} className="minh-50vh d-flex align-items-center">
+                    <Col xs={12} md={7} className="minh-25vh d-none d-md-block"></Col>
+                    <Col xs={12} md={5} className="minh-50vh d-flex align-items-center">
                         <div className="w-100 text-center text-md-start pt-5 text-shadow">
                             <h2 className="dispaly-6 fw-bold text-white-50 text-shadow">
                                 Dolor sit amet         
